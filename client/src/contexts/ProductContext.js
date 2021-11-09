fca447cc5a3acf2885959c40eb5773ff0fb1ce50
NguyenVanLieu0405 <nguyenvanlieu0405@gmail.com>
2021-11-09 10:36:11 +0700

    Add

---
+++ client/src/contexts/ProductContext.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import {createContext,useReducer,useState} from 'react';
+import {productReducer} from '../reducers/productReducer';
+import {
+    apiUrl,PRODUCT_LOAD_SUCCESS,
+    PRODUCT_LOAD_FAIL,PRODUCT_CREATE_SUCCESS,
+    PRODUCT_LOAD_DETAIL_SUCCESS,DELETE_PRODUCT_SUCESS,
+    PRODUCT_UPDATE_SUCCESS
+} from './contants';
+import axios from 'axios';
+
+export const ProductContext=createContext();
+
+const ProductContextProvider = ({children})=>{
+    // [STATE]
+    const [productState,dispatch]=useReducer(productReducer,{
+        product:null,
+        products:[],
+        productsLoading:true,
+        productLoading:true
+    })
+
+    const [showAddCar,setShowAddCar]=useState(false);
+    const [showUpdateCar,setShowUpdateCar]=useState(false);
+    const [showViewCar,setShowViewCar]=useState(false);
+    const [showDelCar,setShowDelCar]=useState({
+        show:false,productId:''
+    });
+    // [GET] PRODUCTS
+    const getProduct=async ()=>{
+        try {
+            const response=await axios.get(`${apiUrl}/products`)
+            if(response.data.success){
+                dispatch({type:PRODUCT_LOAD_SUCCESS,payload:response.data.products})
+            }
+        } catch (e) {
+            dispatch({type:PRODUCT_LOAD_FAIL})
+        }
+    }
+    // [GET] PRODUCT DETAIL
+    const getProductDetail=async (productId)=>{
+        try {
+            const response=await axios.get(`${apiUrl}/products/${productId}`)
+            if(response.data.success){
+                dispatch({type:PRODUCT_LOAD_DETAIL_SUCCESS,payload:response.data.product})
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    // [CREATE] PRODUCT
+    const createProduct=async (productForm)=>{
+        try {
+            const response= await axios.post(`${apiUrl}/products`,productForm);
+            if(response.data.success){
+                dispatch({type:PRODUCT_CREATE_SUCCESS,payload:response.data.product})
+                return response.data;
+            }
+        } catch (e) {
+            return e.response.data ? e.response.data : {success: false,message: 'Lỗi server'}
+        }
+    }
+    // [UPDATE] PRODUCT
+    const updateProduct=async (productForm)=>{
+        try {
+            const response = await axios.put(`${apiUrl}/products/${productForm._id}`,productForm)
+            if(response.data.success){
+                dispatch({type:PRODUCT_UPDATE_SUCCESS,payload:response.data.product})
+                return response.data;
+            }
+        } catch (e) {
+            return e.response.data ? e.response.data : {success: false,message: 'Lỗi server'}
+        }
+    }
+    
+    // [DELETE] PRODUCT
+    const deleteProduct=async (productId)=>{
+        try {
+            const response=await axios.delete(`${apiUrl}/products/${productId}`)
+            console.log(response.data)
+            if(response.data.success){
+                dispatch({type:DELETE_PRODUCT_SUCESS,payload:productId})
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const productContextData={
+        productState,
+        getProduct,createProduct,
+        showAddCar,setShowAddCar,
+        getProductDetail,deleteProduct,
+        showDelCar,setShowDelCar,
+        updateProduct,
+        showUpdateCar,setShowUpdateCar,
+        showViewCar,setShowViewCar
+    }
+
+    return(
+        <ProductContext.Provider value={productContextData}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
+
+export default  ProductContextProvider;
---
+++ client/src/views/components/page/AdminComment.js
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react'
+import {useContext, useState} from 'react'
+import {Row,Col, Container,Card,Table,Spinner,Button} from 'react-bootstrap'
+import {CommentContext} from '../../../contexts/CommentContext'
+import { ProductContext } from '../../../contexts/ProductContext'
+import moment from 'moment';
+export default function AdminComment() {
+    const {getComment,commentState:{comments}}=useContext(CommentContext)
+    const {getProduct,productState:{products,productsLoading}}=useContext(ProductContext)
+    const [stateProductId,setStateProductId]=useState({
+        productId:productsLoading===false&&(products[0]._id)
+    }) 
+    const {productId}=stateProductId
+    useEffect(()=>{
+        getComment(productId)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[productId])
+    useEffect(()=>{
+        getProduct()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+    return (
+        <Container>
+            <Row>
+                <Col xs="3">    
+                <Card style={{ width: '100%' }} border="light">
+                {products.map(product=>(
+                        <Row className="d-flex align-items-center" style={{borderBottom:'1px solid #0091ff' }} onClick={()=>setStateProductId({
+                            productId:product._id
+                        })}>
+                        <Col xs="5"> 
+                             <Card.Img variant="top" src={product.imgCarUrl} />
+                        </Col>
+                        <Col xs="7" style={{padding:'0px'}}>
+                        <Card.Body>
+                          <Card.Title as="h6">{product.nameCar}</Card.Title>
+                         </Card.Body>
+                        </Col>
+                      </Row>
+                  ))} 
+                </Card>
+                </Col>
+                <Col xs="9">
+                <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>STT</th>
+                    <th>Tên khách hàng</th>
+                    <th>Email</th>
+                    <th>Nội dung</th>
+                    <th>Đánh giá</th>
+                    <th>Ngày tạo</th>
+                    <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {comments.map((comment,index)=>(
+                        <tr>
+                    <td>{index+1}</td>
+                    <td>{comment.nameComment}</td>
+                    <td>{comment.emailComment}</td>
+                    <td>{comment.contentComment}</td>
+                    <td>{comment.rate}</td>
+                    <td>{moment(comment.createdAt).format('DD/MM/YYYY HH:mm')}</td>
+                    <td>
+                    <center>
+                                    <div className="d-flex justify-content-around align-items-center">
+                                        <a href={`email:${comment.nameComment}`}>
+                                            <Button variant="outline-primary" size="sm" ><i className="fas fa-email"></i></Button>
+                                        </a>
+                                        <Button variant="outline-primary" size="sm" ><i className="fas fa-trash"></i></Button>
+                                    </div>
+                                </center>
+                    </td>
+                    </tr>
+                    ))}
+                    
+                </tbody>
+                </Table>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
