5d6b4fe45c17a4bb99cc4cbfb609faf1a082cf86
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2021-12-17 23:00:48 +0700

    thay doi error

--- client/src/views/components/page/Error.js
+++ client/src/views/components/page/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Error() {
     return (
         <div>
-            <h1>Oh no! Trang web không tồn tại :( </h1>
+            <h1>Oh no! Trang web không tồn tại :( , Bạn thử liên hệ với lập trình viên</h1>
         </div>
     )
 }
